6bb1fd6d466a8ce971fc8557c1be07dcd615f912
mohamadmrt <mortezaei76@gmail.com>
2022-12-25 16:18:45 +0330

    convert dates to persian date in response

--- app/Http/Resources/v1/CargoCollection.php
+++ app/Http/Resources/v1/CargoCollection.php
@@ -1,46 +1,46 @@
 <?php
 
-    namespace App\Http\Resources\v1;
+namespace App\Http\Resources\v1;
 
-    use App\Amazing;
-    use App\Cargo;
-    use Illuminate\Http\Resources\Json\JsonResource;
+use App\Amazing;
+use App\Cargo;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-    class CargoCollection extends JsonResource
+class CargoCollection extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
     {
-        /**
-         * Transform the resource into an array.
-         *
-         * @param  \Illuminate\Http\Request
-         * @return array
-         */
-        public function toArray($request)
-        {
-            $is_favorite=false;
-            if ($request->bearerToken()){
-                    $user=auth()->guard('api')->user();
-                    if ($user) {
-                        $user_favorites=$user->favorites;
-                        if ($user_favorites->contains('cargo_id',$this->id)){
-                            $is_favorite=true;
-                        }
-                    }
+        $is_favorite = false;
+        if ($request->bearerToken()) {
+            $user = auth()->guard('api')->user();
+            if ($user) {
+                $user_favorites = $user->favorites;
+                if ($user_favorites->contains('cargo_id', $this->id)) {
+                    $is_favorite = true;
+                }
             }
-            return [
-                'id'=>$this->id,
-                'name' => $this->name,
-                'description' => $this->description,
-                'max_count'=>$this->max_count,
-                'image'=>Cargo::getLogo($this->id),
-                'price'=>$this->price,
-                'mfg_date_show'=>$this->mfg_date_show,
-                'exp_date_show'=>$this->exp_date_show,
-                'manufacturing_date'=>$this->manufacturing_date,
-                'expiry_date'=>$this->expiry_date,
-                'main_price'=>$this->main_price_method,
-                'is_favorite'=>$is_favorite,
-                'saleOff'=>$this->is_amazing()?round(100*($this->price-Amazing::where('cargo_id',$this->id)->first()->price_discount)/$this->price,1):round(100*($this->price-$this->price_discount)/$this->price,1),
-
-            ];
         }
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'max_count' => $this->max_count,
+            'image' => Cargo::getLogo($this->id),
+            'price' => $this->price,
+            'mfg_date_show' => $this->mfg_date_show,
+            'exp_date_show' => $this->exp_date_show,
+            'manufacturing_date' => $this->manufacturing_date,
+            'expiry_date' => $this->expiry_date,
+            'main_price' => $this->main_price_method,
+            'is_favorite' => $is_favorite,
+            'saleOff' => $this->is_amazing() ? round(100 * ($this->price - Amazing::where('cargo_id', $this->id)->first()->price_discount) / $this->price, 1) : round(100 * ($this->price - $this->price_discount) / $this->price, 1),
+
+        ];
     }
+}
