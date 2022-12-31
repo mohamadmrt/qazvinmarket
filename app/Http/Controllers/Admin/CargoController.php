166e14e30ee369b42112e595b63176ff28793c18
mohamadmrt <mortezaei76@gmail.com>
2022-12-31 17:19:13 +0330

    fix sync menu parent_id when update cargo category from admin

--- app/Http/Controllers/Admin/CargoController.php
+++ app/Http/Controllers/Admin/CargoController.php
@@ -6,6 +6,7 @@ use App\Cargo;
 use App\Http\Controllers\Admin\Auth\AdminController;
 use App\Http\Resources\v1\ocms\CargoCollection;
 use App\Market;
+use App\Menu;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -22,14 +23,14 @@ class CargoController extends AdminController
     {
         $market = get_market(1);
         $selected = collect([]);
-        if ($request['params']['search_by_name' ]){
-            $cargos = Cargo::searchadmin($request->params)->WhereRaw("MATCH(name) AGAINST('".$request['params']['search_by_name']."')")->get();
-        }else{
+        if ($request['params']['search_by_name']) {
+            $cargos = Cargo::searchadmin($request->params)->WhereRaw("MATCH(name) AGAINST('" . $request['params']['search_by_name'] . "')")->get();
+        } else {
             $cargos = Cargo::searchadmin($request->params)->get();
         }
         if ($request->params['search_by_image'] == "1") {
             foreach ($cargos as $cargo) {
-                if (file_exists(public_path() . '/images/imagefood/' . $market->folder_name . '/' . $cargo->id . '.' . $cargo->file_format.'?v='.$cargo->image_version)) {
+                if (file_exists(public_path() . '/images/imagefood/' . $market->folder_name . '/' . $cargo->id . '.' . $cargo->file_format . '?v=' . $cargo->image_version)) {
                     $selected->push($cargo);
                 }
             }
@@ -117,11 +118,11 @@ class CargoController extends AdminController
 
     public function update_cargo(Request $request, Cargo $cargo)
     {
-        try{
-            if ($request->hasFile('image')){
+        try {
+            if ($request->hasFile('image')) {
                 $image = $request->file('image');
-                $image_name = $cargo->id.'.jpg';
-                $image->move(public_path('/images/imagefood/qazvin/'),$image_name);
+                $image_name = $cargo->id . '.jpg';
+                $image->move(public_path('/images/imagefood/qazvin/'), $image_name);
 
 
 //                $file=$request->File('image');
@@ -130,44 +131,49 @@ class CargoController extends AdminController
             }
 
             $integers = array_map('intval', explode(',', $request->cargo_menu));
-            if (count($integers)>0 and $integers[0]!=0)
-                $cargo->menus()->sync($integers);
+            if (count($integers) > 0 and $integers[0] != 0)
+                \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            $cargo->menus()->detach();
+            foreach ($integers as $item) {
+                $menu = Menu::find($item);
+                $cargo->menus()->attach([$item => ['pivot_parent_id' => $menu->parent_id]]);
+            }
 
 
-            if ($request->mfg_date !== null){
+            if ($request->mfg_date !== null) {
                 $mfg_date = CalendarUtils::createCarbonFromFormat('Y/m/d  h:i:s', convertPersianToEnglish($request->mfg_date));
-                $cargo->manufacturing_date=$mfg_date;
+                $cargo->manufacturing_date = $mfg_date;
             }
-            if ($request->exp_date !== null){
+            if ($request->exp_date !== null) {
                 $exp_date = CalendarUtils::createCarbonFromFormat('Y/m/d  h:i:s', convertPersianToEnglish($request->exp_date));
-                $cargo->expiry_date=$exp_date;
+                $cargo->expiry_date = $exp_date;
             }
 
 
-            $cargo->description=$request->description;
-            $cargo->max_order=$request->max_order;
-            $cargo->max_count=$request->max_count;
+            $cargo->description = $request->description;
+            $cargo->max_order = $request->max_order;
+            $cargo->max_count = $request->max_count;
 
-            $cargo->file_format='jpg';
-            $cargo->price_discount=$request->price_discount;
-            $cargo->newest=$request->newest=="true"?"1":"0";
-            if ($request->newest=="true"){
-                $cargo->newest_order=Carbon::now();
+            $cargo->file_format = 'jpg';
+            $cargo->price_discount = $request->price_discount;
+            $cargo->newest = $request->newest == "true" ? "1" : "0";
+            if ($request->newest == "true") {
+                $cargo->newest_order = Carbon::now();
             }
-            $cargo->status=$request->status=="true"?"1":"0";
-            $cargo->mfg_date_show=$request->mfg_date_show=="true"?1:0;
-            $cargo->exp_date_show=$request->exp_date_show=="true"?1:0;
+            $cargo->status = $request->status == "true" ? "1" : "0";
+            $cargo->mfg_date_show = $request->mfg_date_show == "true" ? 1 : 0;
+            $cargo->exp_date_show = $request->exp_date_show == "true" ? 1 : 0;
             $cargo->image_version = $cargo->image_version + 1;
             $cargo->save();
             return response([
-                'data'=>[],
-                'status'=>'ok'
+                'data' => [],
+                'status' => 'ok'
             ]);
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             return response([
-                'data'=>$e->getMessage(),
-                'status'=>'fail'
-            ],422);
+                'data' => $e->getMessage(),
+                'status' => 'fail'
+            ], 422);
         }
     }
 }
