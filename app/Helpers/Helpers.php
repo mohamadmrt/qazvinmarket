7ea217d7ad300d76f9ec8784e25ab8bac58d85f0
mohamadmrt <mortezaei76@gmail.com>
2023-02-15 12:36:20 +0330

    adding error for unsuccess payment in mellat pay method to sms for mrt

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Market;
+use App\Sms;
 use Hashids\Hashids;
 use Illuminate\Support\Facades\Session;
 use Morilog\Jalali\CalendarUtils;
@@ -154,10 +155,12 @@ if (!function_exists('verify_payment')) {
                 if ($settle == 0) {
                     return 0;
                 } else {
+                    $sms = Sms::insert('10', '09300606049', 'error in settel,saleRefrenceId:'.$SaleReferenceId.'-saleOrderId:'.$SaleOrderId);
                     $client->bpReversalRequest($parameters);
                     return 1;
                 }
             } else {
+                $sms = Sms::insert('10', '09300606049', 'error in verify,saleRefrenceId:'.$SaleReferenceId.'-saleOrderId:'.$SaleOrderId);
                 $client->bpReversalRequest($parameters);
                 return 1;
             }
